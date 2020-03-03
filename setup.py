81b62aca83425bda0bda865340047db7050782c7
zhuzi <zhul9311@gmail.com>
2020-03-02 19:00:38 -0600

    [M]: Windows test passed. Version changed to 0.2.2. Ready to publish.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = "xfntr",
-    version = "0.2.1",
+    version = "0.2.2",
     author = "Zhu Liang",
     author_email = "zliang8@uic.edu",
     description = "A software that analyzes xfntr data",
